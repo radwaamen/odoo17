23f809fc0cdd47f9d473cd39c3d75df31a5087bd
Radwa Test <Radwaamen0@gmail.com>
2025-07-01 11:44:10 +0300

    update

---
+++ sale_print/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Sale Print",
+    'author': "Beshoy Wageh",
+    'website': "https://www.linkedin.com/in/beshoy-wageh-701ba7116/",
+    'depends': ['base', 'sale','purchase'],
+    'data': [
+        'views/header_custom.xml',
+        'views/templates.xml',
+        'views/sales.xml',
+    ],
+}
